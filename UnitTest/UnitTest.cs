db7590d638b0e346a81e2f26a6d5ade57e39fa9d
Lingxi Li <lingxl@microsoft.com>
2022-07-08 19:03:09 +0800

    Improve subname matching

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -14,7 +14,7 @@ namespace UnitTest
             {
                 "上海市浦东医院复旦大学附属华山医院南汇分院(原:上海市浦东新区中心医院)"
             };
-            var name = "复旦大学附属华山医院东院";
+            var name = "十堰市太和医院/湖北医药学院(原:郧阳医学院)附属太和医院";
             var engine = new LabelEngine(labels);
             var match = engine.TryLabel(name);
             Console.WriteLine(match.OriginalEntry);
